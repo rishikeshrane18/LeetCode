9a07c95c43047808f4dd85500ff6eff085586280
Rishikesh Rane <62653435+rishikeshrane18@users.noreply.github.com>
2022-05-28 02:32:05 +0530

    Update 206-reverse-linked-list.java

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -11,29 +11,29 @@
 
         ///// ITERATIVE APPROACH TO REVERSE
 
-// class Solution {
-//     public ListNode reverseList(ListNode head) {
-//         if(head == null || head.next == null){
-//                 return head;
-//             }
-//         ListNode prevNode = head;
-//         ListNode currNode = head.next;
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null){
+                return head;
+            }
+        ListNode prevNode = head;
+        ListNode currNode = head.next;
         
-//         while(currNode != null){
+        while(currNode != null){
             
-//             ListNode nextNode = currNode.next;
+            ListNode nextNode = currNode.next;
             
-//             currNode.next = prevNode;
-//             prevNode = currNode;
-//             currNode = nextNode;
-//         }
-//         head.next = null;
-//         head = prevNode;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
         
-//         return head;
-//     }
+        return head;
+    }
    
-// }
+ }
 
 
 
