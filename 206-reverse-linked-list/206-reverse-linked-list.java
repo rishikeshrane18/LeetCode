1284ecf3abd19fd5e845506effd55981d14bfa01
Rishikesh Rane <62653435+rishikeshrane18@users.noreply.github.com>
2022-05-28 02:28:32 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (52.83%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -8,30 +8,57 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+        ///// ITERATIVE APPROACH TO REVERSE
+
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if(head == null || head.next == null){
+//                 return head;
+//             }
+//         ListNode prevNode = head;
+//         ListNode currNode = head.next;
+        
+//         while(currNode != null){
+            
+//             ListNode nextNode = currNode.next;
+            
+//             currNode.next = prevNode;
+//             prevNode = currNode;
+//             currNode = nextNode;
+//         }
+//         head.next = null;
+//         head = prevNode;
+        
+//         return head;
+//     }
+   
+// }
+
+
+
+
+
+
+
+
+
+
+// RECURSIVE APPROACH
+
 class Solution {
-    
     public ListNode reverseList(ListNode head) {
         
-        // ITERATIVE SOLUTION 
-        
         if(head == null || head.next == null){
-                return head;
-            }
-        ListNode prevNode = head;
-        ListNode currNode = head.next;
-        
-        while(currNode != null){
-            
-            ListNode nextNode = currNode.next;
-            
-            currNode.next = prevNode;
-            prevNode = currNode;
-            currNode = nextNode;
+            return head;
         }
-        head.next = null;
-        head = prevNode;
         
-        return head;
+        ListNode newhead = reverseList(head.next);
+        
+        head.next.next = head;
+        head.next = null;
+        
+        return newhead;
+        
     }
-   
 }
